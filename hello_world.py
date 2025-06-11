0e3f92c22406d4ca3c66fecae0144f6e340324b8
Jada Brown <jbrowbb@siue.edu>
2025-06-11 18:51:30 -0500

    changing file

--- hello_world.py
+++ hello_world.py
@@ -11,5 +11,10 @@ def factorial(n):
     
 
 for _ in range(3):
-    num = random.randint(1,10)
+    num = random.randint(1,30)
     print(f"Factorial of {num}: {factorial(num)}")
+
+
+print("I dont think i did the merge / conflict one correct")
+
+
